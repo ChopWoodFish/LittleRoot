fc5ac79e6c4077fe3bba157c088fbc8e766700dc
xiaoxichen <xiaoxi.chen@centurygame.com>
2023-02-04 14:05:10 +0800

    PlayerController脚本 基础左右移动

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed = 1f;
+
+    private float scaleX;
+    private float scaleY;
+
+    private void Awake()
+    {
+        scaleX = transform.localScale.x;
+        scaleY = transform.localScale.y;
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        // -1, 0, 1
+        float horizontalMove = Input.GetAxisRaw("Horizontal");
+        Debug.Log($"horizontalMove: {horizontalMove}");
+
+        // 不要惯性
+        // if (horizontalMove != 0)
+        // {
+            rb.velocity = new Vector2(horizontalMove * speed, rb.velocity.y);
+            // 转身
+            if (horizontalMove != 0)
+            {
+                transform.localScale = new Vector3(horizontalMove * scaleX, scaleY, 1);   
+            }
+            // }
+
+    }
+}

19c1c13240aefae8ecc98ab7e113f3603418d285
Candice <candice2dlt@gmail.com>
2023-02-04 16:14:49 +0800

    追捕者跟随

---
+++ Assets/Scripts/PersuerChase.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersuerChase : MonoBehaviour
+{
+    public float moveSpeed = 3.0f;
+    private Vector2 movDir = new Vector2(); //物理引擎方式采用的参数
+    public Transform target; //要追捕的对象
+    private Rigidbody2D rb2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Chase();
+    }
+
+    private void Chase(){
+        movDir.x = Input.GetAxisRaw("Horizontal");
+        movDir.y = Input.GetAxisRaw("Vertical");
+ 
+        //防止角色斜向运动过快，我们让斜方向距离也为1，原来是根号2
+        //归一化处理
+        movDir.Normalize();
+
+        //角色向某个位置移动（不是受控制的移动）可以实现跟随或小怪追主角
+        transform.position = Vector2.MoveTowards(transform.position,target.position,Time.deltaTime);
+    }
+}

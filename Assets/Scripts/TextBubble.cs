ddced2ff140e57606a7ed672587008d3af962492
Candice <candice2dlt@gmail.com>
2023-02-05 14:26:17 +0800

    bubble test

---
+++ Assets/Scripts/TextBubble.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextBubble : MonoBehaviour
+{
+    public Transform targetVillager;
+    public GameObject TextBubbles;//测试在转成屏幕的位置生成一个对话气泡
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector2 screenPos = Camera.main.WorldToScreenPoint(targetVillager.position); 
+
+    
+        var newBubble = Instantiate(TextBubbles);
+        newBubble.transform.position = screenPos;
+    }
+}
